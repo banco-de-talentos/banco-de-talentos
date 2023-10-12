0c6bcbec34609e5ded5e83065bace6eca99d104f
Amanda Portela <mandaptela@gmail.com>
2023-10-11 21:26:50 -0300

    feat: diretórios e configurações iniciais do sequelize

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -2,4 +2,9 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request: any, response: any) => {
+    return response.json({ message: 'Hello asasdas!'});
+  });
+  
+
 module.exports = app;
