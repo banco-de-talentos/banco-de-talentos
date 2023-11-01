b3c5daea1109c3acf1a700dc77718a1f7632e65b
Tassio-Med <tassiohfmed@gmail.como>
2023-11-01 16:50:51 -0300

    feat: páginas criadas

---
+++ frontend/src/pages/PageForm1/index.tsx
@@ -0,0 +1,7 @@
+export const PageForm1 = () => {
+  return(
+    <div>
+      <h1>Página 1</h1>
+    </div>
+  )
+}
---
+++ frontend/src/pages/PageForm3/index.tsx
@@ -0,0 +1,7 @@
+export const PageForm3 = () => {
+  return(
+    <div>
+      <h1>Página 3</h1>
+    </div>
+  )
+}
