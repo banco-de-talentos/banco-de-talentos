cd8139b83702d10888b1459b82353f4cea5c0240
Amanda Portela <mandaptela@gmail.com>
2023-11-02 18:26:01 -0300

    ajuste de código conflituoso

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,11 @@
 import './assets/styles/App.css';
 import PageRouter from './routes/router';
 
-import './App.css'
-import { RouterForm } from './routes/router'
-
-
 function App() {
 
   return (
     <>
-
-      <PageRouter/>
-
-      <RouterForm/>
-
+      <PageRouter />
     </>
   )
 }
