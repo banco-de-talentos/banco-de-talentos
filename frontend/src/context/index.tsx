461da46656f8967095f2dc1b6189dcf7f7c910e6
Tassio-Med <tassiohfmed@gmail.como>
2023-11-03 17:05:28 -0300

    feat: context e seus tipos implemantado

---
+++ frontend/src/context/index.tsx
@@ -0,0 +1,51 @@
+
+import {  createContext, Reducer, useReducer} from "react";
+import { State, FormProps, FormStates, Action, ContextTypes } from "../types";
+
+const initialData: State = {
+  currentPage: 0,
+  name: '',
+  email: '',
+  github: '',
+  linkedin: '',
+  telefone: '',
+  level: 0,
+}
+
+
+export const FormContext = createContext<ContextTypes | undefined>(undefined);
+
+
+const formReducer: Reducer<State, Action> = (state, action) => {
+  switch(action.type){
+    case FormStates.setCurrentPage:
+      return { ...state, currentStep: action.payload };
+    case FormStates.setName:
+      return {...state, name: action.payload };
+    case FormStates.setEmail:
+      return { ...state, email: action.payload};
+    case FormStates.setGithub:
+      return { ...state, github: action.payload };
+    case FormStates.setLinkedin:
+      return { ...state, linkedin: action.payload };
+    case FormStates.setTelefone:
+      return { ...state, telefone: action.payload };
+    case FormStates.setLevel:
+      return { ...state, level: action.payload };
+    default:
+      return state;
+    }
+}
+
+// Provider
+
+export const FormProvider = ({children}: FormProps) => {
+  const [state, dispatch] = useReducer(formReducer, initialData);
+  const value = { state, dispatch };
+
+  return(
+    <FormContext.Provider value={value}>
+      { children }
+    </FormContext.Provider>
+  )
+}
---
+++ frontend/src/types/index.tsx
@@ -0,0 +1,57 @@
+import { ReactNode, /* MouseEventHandler */  } from "react";
+
+export enum FormStates {
+  setCurrentPage,
+  setName,
+  setEmail,
+  setGithub,
+  setTelefone,
+  setLinkedin,
+  setLevel,
+} 
+
+export type State = {
+  currentPage: number,
+  name: string,
+  email: string,
+  github: string,
+  telefone: string,
+  linkedin: string,
+  level: number,
+}
+
+export type Action = {
+  type: FormStates;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  payload: any;
+}
+
+export type ContextTypes = {
+  state: State;
+  dispatch: (action: Action) => void;
+}
+
+export type FormProps = {
+  children: ReactNode;
+}
+
+// export type SelectProps = {
+//   isselected: boolean,
+//   icon: string,
+//   title: string,
+//   description: string,
+//   onClick: () => void;
+// }
+
+// export type SidebarProps = {
+//   icon: string,
+//   title: string,
+//   description: string,
+//   path: string,
+//   selected: boolean;
+// }
+
+// export type ButtonProps = {
+//   prevLink: string,
+//   onNextClick: MouseEventHandler<HTMLButtonElement>;
+// }
