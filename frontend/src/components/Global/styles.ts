b28ae78cc858c736f63348cd81137f12b0e3b769
Tassio-Med <tassiohfmed@gmail.como>
2023-11-09 15:32:39 -0300

    feat: estrutura do componente Global iniciado e estilizado

---
+++ frontend/src/components/Global/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #0b090a;
+  color: #fff;
+  color: #FFF;
+  min-height: 100vh;
+`;
+  
+export const Area = styled.div`
+  margin: auto;
+  padding: 25px;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Steps = styled.div`
+  flex: 1;
+  display: flex;
+`;
+
+export const Sidebar = styled.div`
+  width: 250px;
+  border-right: 1px solid #1C1719;
+`;
+
+export const Page = styled.div`
+  flex: 1;
+  padding-left: 40px;
+  padding-top: 40px;
+`;
