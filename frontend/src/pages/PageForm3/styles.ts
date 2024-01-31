ae954fd1f32d9d8e6771a7a007421ea08f8e8572
Tassio-Med <tassiohfmed@gmail.como>
2024-01-31 17:33:30 -0300

    feat: criada e estilizada a página 03

---
+++ frontend/src/pages/PageForm3/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    p {
+        font-size: 13px;
+        color: #B8B8D4;
+    }
+    h1 {
+        margin: 0;
+        padding: 0;
+        font-size: 26px;
+    }
+    hr {
+        height: 1px;
+        border: 0;
+        background-color: #1C1719;
+        margin: 30px 0;
+    }
+
+    label {
+        font-size: 13px;
+
+        input {
+            display: block;
+            margin-top: 7px;
+            box-sizing: border-box;
+            width: 100%;
+            padding: 20px 10px;
+            border: 2px solid #660708;
+            border-radius: 10px;
+            color: #FFF;
+            outline: 0;
+            font-size: 15px;
+            background-color: #0b090a;
+        }
+    }
+`;
